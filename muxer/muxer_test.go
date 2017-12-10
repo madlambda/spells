ffc92ab57a6e018b18099efdc53b04ff8c9537a7
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-12-10 19:37:52 -0200

    Generalize test

--- muxer/muxer_test.go
+++ muxer/muxer_test.go
@@ -7,17 +7,16 @@ import (
 	"github.com/madlambda/spells/muxer"
 )
 
-func TestSingleIntChannel(t *testing.T) {
-	output := make(chan int)
-	input := make(chan int)
-	expectedOutput := 666
-
-	assert.NoError(t, muxer.Do(output, input))
-	go func() {
-		input <- expectedOutput
-	}()
-	outVal := <-output
-	assert.EqualInts(t, expectedOutput, outVal)
+func TestMux(t *testing.T) {
+	for _, tcase := range []TestCase{
+		TestCase{
+			name:            "oneInput",
+			expectedOutputs: []int{666},
+			inputChannels:   1,
+		},
+	} {
+		testMux(t, tcase)
+	}
 }
 
 func TestMultipleIntChannels(t *testing.T) {
@@ -30,4 +29,36 @@ func TestErrorOnInvalidInput(t *testing.T) {
 }
 
 func TestErrorOnIncompatibleInputsOutputs(t *testing.T) {
+}
+
+type TestCase struct {
+	name            string
+	expectedOutputs []int
+	inputChannels   int
+}
+
+func testMux(t *testing.T, tcase TestCase) {
+	t.Run(tcase.name, func(t *testing.T) {
+		inputs := []chan int{}
+		inputsgen := []interface{}{}
+		for i := 0; i < tcase.inputChannels; i++ {
+			input := make(chan int)
+			inputs = append(inputs, input)
+			inputsgen = append(inputsgen, input)
+		}
+		output := make(chan int)
+		assert.NoError(t, muxer.Do(output, inputsgen...))
+
+		go func() {
+			for i, v := range tcase.expectedOutputs {
+				inindex := i % len(inputs)
+				inputs[inindex] <- v
+			}
+		}()
+
+		for _, want := range tcase.expectedOutputs {
+			got := <-output
+			assert.EqualInts(t, want, got)
+		}
+	})
 }
