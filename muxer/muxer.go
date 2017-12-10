dcd86106664c1e616e64fbf046664e6f6e0e70cc
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-12-10 19:22:56 -0200

    First passing test

--- muxer/muxer.go
+++ muxer/muxer.go
@@ -4,15 +4,29 @@
 // channels on just one.
 package muxer
 
+import "reflect"
+
 // Do will mux all the provided inputs channels on the given
 // output channel. Both are interface{} since this will
 // mux any type of channel (a point for generics =/).
 //
+// A goroutine is created to perform the muxing.
 // It is a severe programming error to call this function
 // with a parameter that is not a channel or with channels
 // of different types.
 //
 // Nil channels will be ignored.
 func Do(output interface{}, inputs ...interface{}) error {
+	// TODO type checking
+	// TODO channel direction checking
+	// TODO empty inputs checking
+	outputVal := reflect.ValueOf(output)
+	inputVal := reflect.ValueOf(inputs[0])
+
+	go func() {
+		v, _ := inputVal.Recv()
+		outputVal.Send(v)
+	}()
+
 	return nil
 }
