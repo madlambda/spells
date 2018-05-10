1445165207e49a6e20053c076de5561d4096c06e
i4k <tiago4orion@gmail.com>
2018-05-09 21:35:14 -0300

    add EqualErrors and EqualFloats
    Signed-off-by: i4k <tiago4orion@gmail.com>

--- assert/equal.go
+++ assert/equal.go
@@ -2,8 +2,11 @@ package assert
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
+
+var ε = math.Nextafter(1, 2) - 1
 
 func EqualStrings(t *testing.T, want string, got string, details ...interface{}) {
 	t.Helper()
@@ -21,6 +24,39 @@ func EqualInts(t *testing.T, want int, got int, details ...interface{}) {
 	}
 }
 
+func EqualFloats(
+	t *testing.T, want, got float64, details ...interface{},
+) {
+	t.Helper()
+
+	if !floatEqual(want, got) {
+		detail := errordetails(details...)
+		t.Fatalf("wanted[%f] but got[%f].%s",
+			want, got, detail)
+	}
+}
+
+func EqualErrs(
+	t *testing.T, want, got error, details ...interface{},
+) {
+	t.Helper()
+
+	detail := errordetails(details...)
+	if got != nil {
+		if want != nil {
+			if got.Error() != want.Error() {
+				t.Fatalf("wanted[%s] but got[%s].%s", want,
+					got, detail)
+			}
+		}
+
+		t.Fatalf("got unexpected error[%s].%s", got, detail)
+	} else if want != nil {
+		t.Fatalf("expected error[%s] but got nil.%s",
+			want, detail)
+	}
+}
+
 func errordetails(details ...interface{}) string {
 	if len(details) == 1 {
 		return details[0].(string)
@@ -30,4 +66,8 @@ func errordetails(details ...interface{}) string {
 		return fmt.Sprintf(details[0].(string), details[1:]...)
 	}
 	return ""
+}
+
+func floatEqual(a, b float64) bool {
+	return math.Abs(a-b) < ε && math.Abs(b-a) < ε
 }
