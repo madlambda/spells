028d222765134c05ba731488f79222a9d038372a
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-07-04 16:10:52 -0300

    dont call Fatalf on goroutine

--- semaphore/semaphore_test.go
+++ semaphore/semaphore_test.go
@@ -1,19 +1,20 @@
 package semaphore_test
 
 import (
-	"fmt"
-	"time"
-	"testing"
 	"context"
+	"errors"
+	"fmt"
 	"runtime"
-	
+	"testing"
+	"time"
+
 	"github.com/madlambda/spells/assert"
 	"github.com/madlambda/spells/semaphore"
 )
 
 func TestSemaphore(t *testing.T) {
-	sizes := []uint{ 1, 2, 3, 1000 }
-	
+	sizes := []uint{1, 2, 3, 1000}
+
 	for _, size := range sizes {
 		t.Run(fmt.Sprintf("Size%d", size), func(t *testing.T) {
 			s := semaphore.New(size)
@@ -24,12 +25,12 @@ func TestSemaphore(t *testing.T) {
 			testSemaphore(t, s, size)
 		})
 	}
-	
+
 }
 
 func TestSemaphoreSizeCantBeZero(t *testing.T) {
 	defer assertPanic(t, "expected panic creating semaphore with size 0")
-    semaphore.New(0)
+	semaphore.New(0)
 }
 
 func TestSemaphoreCantReleaseSameAcquireTwice(t *testing.T) {
@@ -37,7 +38,7 @@ func TestSemaphoreCantReleaseSameAcquireTwice(t *testing.T) {
 
 	s := semaphore.New(1)
 	release, err := s.Acquire(context.Background())
-	
+
 	assert.NoError(t, err)
 	release()
 	release()
@@ -47,46 +48,51 @@ func assertPanic(t *testing.T, errmsg string) {
 	r := recover()
 	if r == nil {
 		t.Error(errmsg)
-    }
-    if _, ok := r.(runtime.Error); ok {
-    	t.Errorf("Unexpected runtime error[%s]", r)
-    }
+	}
+	if _, ok := r.(runtime.Error); ok {
+		t.Errorf("Unexpected runtime error[%s]", r)
+	}
 }
 
 func testSemaphore(t *testing.T, s semaphore.S, size uint) {
-	
+
 	releases := []semaphore.Release{}
-	
+
 	for i := uint(0); i < size; i++ {
 		release, err := s.Acquire(context.Background())
 		assert.NoError(t, err)
 		releases = append(releases, release)
 	}
-	
-	timeoutWorked := make(chan struct{})
+
+	timeoutWorked := make(chan error)
 	go func() {
 		timeout := 100 * time.Millisecond
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
-		
+
 		_, err := s.Acquire(ctx)
-		assert.Error(t, err)
-		if ctx.Err() != context.DeadlineExceeded {
-			t.Fatalf("expected deadline exceeded on acquire context, got[%s]", ctx.Err())
+		if err == nil {
+			timeoutWorked <- errors.New("expected timeout error, got none")
+			return
 		}
-		timeoutWorked <- struct{}{}
+		if ctx.Err() != context.DeadlineExceeded {
+			timeoutWorked <- fmt.Errorf("expected deadline exceeded on acquire context, got[%s]", ctx.Err())
+			return
+		}
+		timeoutWorked <- nil
 	}()
-	
-	<-timeoutWorked
-	
+
+	err := <-timeoutWorked
+	assert.NoError(t, err)
+
 	releases[0]()
 	releases = releases[1:]
-	
+
 	r, err := s.Acquire(context.Background())
 	assert.NoError(t, err)
-	
+
 	releases = append(releases, r)
-	
+
 	for _, release := range releases {
 		release()
 	}
