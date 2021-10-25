8d1714cc7dd091343b45e3a33288b97daf4248ef
Tiago Natel <t.nateldemoura@gmail.com>
2021-10-25 12:23:38 +0100

    feat: Introduces utf8.Reader interface.
    Introduces utf8.Reader interface and implementations for reading
    runes from io.RuneReader and io.Reader interfaces.

---
+++ utf8/reader.go
@@ -0,0 +1,143 @@
+package utf8
+
+import (
+	"fmt"
+	"io"
+	"unicode/utf8"
+)
+
+type (
+	reader struct {
+		r io.RuneReader
+	}
+
+	ReaderReader struct {
+		r io.Reader
+	}
+
+	Reader interface {
+		Read(data []rune) (int, error)
+	}
+)
+
+func NewReader(r io.RuneReader) Reader {
+	return &reader{
+		r: r,
+	}
+}
+
+func (rd *reader) Read(data []rune) (int, error) {
+	for i := 0; i < len(data); i++ {
+		r, _, err := rd.r.ReadRune()
+		if err != nil {
+			return i, err
+		}
+
+		data[i] = r
+	}
+
+	return len(data), nil
+}
+
+// NewReaderReader creates a utf8.Reader implementation from an io.Reader
+// interface.
+func NewReaderReader(r io.Reader) Reader {
+	return &ReaderReader{
+		r: r,
+	}
+}
+
+// Read implements utf8.Reader interface.
+//
+// It will use O(N) space where N is the size of the data slice provided.
+// For ASCII data it allocates exactly len(data) bytes (or sizeof(data)/4) but
+// in case of multi-byte code points it allocates a maximum of sizeof(data)
+// or len(data)*4.
+//
+// The first call to the source reader uses a full buffer of len(data) bytes
+// but subsequent calls read byte by byte. This is a tradeoff optimization for
+// the kind of use cases this function was designed for: parsing languages.
+// Configuration files and programming languages source code are mainly composed
+// of ASCII data, so allocating len(data)*4 seems to be wasteful.
+func (rr *ReaderReader) Read(data []rune) (int, error) {
+	var (
+		nrunes int
+		start  int
+	)
+
+	end := len(data)
+	b := make([]byte, 0, end)
+
+	for nrunes < len(data) {
+		if len(b) == cap(b) {
+			b = append(b, 0)[:len(b)]
+			end = len(b) + 1
+		}
+
+		begin := len(b)
+
+		n, err := rr.r.Read(b[begin:end])
+
+		b = b[:len(b)+n]
+
+		if n > 0 {
+			count := lastPartialCount(b[start : begin+n])
+
+			for start+count < begin+n {
+				r, size := utf8.DecodeRune(b[start : begin+n])
+				if r == utf8.RuneError {
+					return nrunes, fmt.Errorf("invalid rune")
+				}
+
+				start += size
+				data[nrunes] = r
+				nrunes++
+			}
+
+			if start+count == begin+n && len(b) == end {
+				end++
+			}
+		}
+
+		if err != nil {
+			return nrunes, err
+		}
+	}
+
+	return nrunes, nil
+}
+
+func lastPartialCount(p []byte) int {
+	// Look for final start of rune.
+	for i := 0; i < len(p) && i < utf8.UTFMax; {
+		i++
+		if utf8.RuneStart(p[len(p)-i]) {
+			if utf8.FullRune(p[len(p)-i:]) {
+				return 0
+			}
+			// Found i bytes of partial rune.
+			return i
+		}
+	}
+	// Did not find start of final rune - invalid or empty input.
+	return 0
+}
+
+// ReadAll reads from r until an error or EOF and returns the data.
+func ReadAll(r Reader) ([]rune, error) {
+	b := make([]rune, 0, 512)
+	for {
+		if len(b) == cap(b) {
+			// Add more capacity (let append pick how much).
+			b = append(b, 0)[:len(b)]
+		}
+		n, err := r.Read(b[len(b):cap(b)])
+		b = b[:len(b)+n]
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return b, err
+		}
+	}
+}
---
+++ utf8/reader_test.go
@@ -0,0 +1,224 @@
+package utf8_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	stdiotest "testing/iotest"
+
+	"github.com/madlambda/spells/assert"
+	"github.com/madlambda/spells/iotest"
+	"github.com/madlambda/spells/utf8"
+)
+
+const expectedString = "Εν οίδα ότι ουδέν οίδα"
+
+func TestUTF8Reader(t *testing.T) {
+	type testcase struct {
+		input  string
+		repeat int
+		err    error
+	}
+
+	testcases := []testcase{
+		{
+			input:  "",
+			repeat: 1,
+		},
+		{
+			input:  "test",
+			repeat: 1,
+		},
+		{
+			input:  "Εν οίδα ότι ουδέν οίδα",
+			repeat: 1,
+		},
+		{
+			input:  "Εν οίδα ότι TESTE ουδέν οίδα",
+			repeat: 1,
+		},
+		{
+			input:  "test\n",
+			repeat: 1024,
+		},
+		{
+			input:  "Εν οίδα ότι ουδέν οίδα",
+			repeat: 1024,
+		},
+		{
+			input:  "Εν οίδα ότι ουδέν οίδα",
+			repeat: 1025,
+		},
+		{
+			input:  "Εν οίδα ότι ουδέν οίδα",
+			repeat: 2048,
+		},
+		{
+			input:  "Εν οίδα ότι ουδέν οίδα",
+			repeat: 2049,
+		},
+	}
+
+	for _, tc := range testcases {
+		reader := utf8.NewReaderReader(iotest.NewRepeatReader(
+			bytes.NewBuffer([]byte(tc.input)),
+			tc.repeat))
+
+		got, err := utf8.ReadAll(reader)
+		assert.NoError(t, err, "reading utf8 reader")
+
+		repeater := iotest.NewRepeatReader(bytes.NewBuffer([]byte(tc.input)),
+			tc.repeat)
+
+		expectedBytes, err := io.ReadAll(repeater)
+		assert.NoError(t, err, "repeating expected")
+
+		expected := []rune(string(expectedBytes))
+
+		assert.EqualErrs(t, tc.err, err, "Read() error")
+		assert.EqualInts(t, len(expected), len(got), "rune slice size mismatch")
+
+		for i, r := range expected {
+			if r != got[i] {
+				t.Errorf("want[%c] but got[%c]", r, got[i])
+			}
+		}
+	}
+
+	// TODO(i4k): implement stdiotest.TestReader for utf8.Reader
+}
+
+func TestUTF8ReaderMultipleSizes(t *testing.T) {
+	for i := 0; i < 2000; i++ {
+		buf1 := bytes.NewBuffer([]byte(expectedString))
+		buf2 := bytes.NewBuffer([]byte(expectedString))
+		repeater1 := iotest.NewRepeatReader(buf1, i)
+		repeater2 := iotest.NewRepeatReader(buf2, i)
+
+		runes, err := utf8.ReadAll(utf8.NewReaderReader(repeater1))
+		assert.NoError(t, err, "reading runes")
+
+		expectedBytes, err := io.ReadAll(repeater2)
+		assert.NoError(t, err, "reading expected runes")
+
+		expected := []rune(string(expectedBytes))
+
+		assert.EqualInts(t, len(expected), len(runes), "length mismatch")
+
+		for j := 0; j < len(expected); j++ {
+			if expected[j] != runes[j] {
+				t.Errorf("rune %d are not equal: %c != %c",
+					j, expected[j], runes[j])
+			}
+		}
+	}
+}
+
+func TestUTF8ReaderHalfRead(t *testing.T) {
+	for i := 0; i < 2000; i++ {
+		buf1 := bytes.NewBuffer([]byte(expectedString))
+		buf2 := bytes.NewBuffer([]byte(expectedString))
+		repeater1 := stdiotest.HalfReader(iotest.NewRepeatReader(buf1, i))
+		repeater2 := iotest.NewRepeatReader(buf2, i)
+
+		runes, err := utf8.ReadAll(utf8.NewReaderReader(repeater1))
+		assert.NoError(t, err, "reading runes")
+
+		expectedBytes, err := io.ReadAll(repeater2)
+		assert.NoError(t, err, "reading expected runes")
+
+		expected := []rune(string(expectedBytes))
+
+		assert.EqualInts(t, len(expected), len(runes), "length mismatch")
+
+		for j := 0; j < len(expected); j++ {
+			if expected[j] != runes[j] {
+				t.Errorf("rune %d are not equal: %c != %c",
+					j, expected[j], runes[j])
+			}
+		}
+	}
+}
+
+func TestUTF8ReaderFromFileMultipleBufferSizes(t *testing.T) {
+
+	expectedRunes := []rune(expectedString)
+	expectedLen := len(expectedRunes)
+
+	temp, err := ioutil.TempFile("", "spells-utf8")
+	assert.NoError(t, err, "creating temp file")
+
+	defer os.Remove(temp.Name())
+
+	n, err := temp.WriteString(expectedString)
+	assert.NoError(t, err, "writing utf8 file")
+	assert.EqualInts(t, len(expectedString), n, "written len mismatch")
+
+	for i := 0; i < expectedLen; i++ {
+		t.Run(fmt.Sprintf("using buffer size: %d", i), func(t *testing.T) {
+			off, err := temp.Seek(0, 0)
+			assert.NoError(t, err, "seek to file offset 0")
+			assert.EqualInts(t, 0, int(off), "invalid offset")
+
+			r := utf8.NewReaderReader(temp)
+
+			runes := make([]rune, i)
+			n, err = r.Read(runes[:])
+
+			assert.NoError(t, err, "failed reading runes")
+			assert.EqualInts(t, i, n, "read wrong number of runes")
+			assert.EqualStrings(t, string(expectedRunes[0:i]), string(runes[0:n]),
+				"wrong read utf8 string")
+		})
+	}
+}
+
+func TestUTF8ReaderNonEOF(t *testing.T) {
+	buf := stdiotest.DataErrReader(bytes.NewBuffer([]byte("test")))
+	reader := utf8.NewReaderReader(buf)
+
+	data := make([]rune, 10)
+	n, err := reader.Read(data[:])
+	assert.EqualInts(t, 4, n, "read size mismatch")
+	assert.EqualErrs(t, io.EOF, err, "error is not EOF")
+	assert.EqualStrings(t, "test", string(data[:n]), "string mismatch")
+}
+
+func TestUTF8ReaderError(t *testing.T) {
+	expected := fmt.Errorf("some error")
+	buf := stdiotest.ErrReader(expected)
+	reader := utf8.NewReaderReader(buf)
+
+	data := make([]rune, 10)
+	n, err := reader.Read(data[:])
+	assert.EqualErrs(t, expected, err, "err mismatch")
+	assert.EqualInts(t, 0, n, "read size mismatch")
+}
+
+func TestUTF8ReaderOneByteReader(t *testing.T) {
+	buf1 := bytes.NewBuffer([]byte(expectedString))
+	buf2 := bytes.NewBuffer([]byte(expectedString))
+	repeater1 := stdiotest.HalfReader(iotest.NewRepeatReader(buf1, 100))
+	repeater2 := iotest.NewRepeatReader(buf2, 100)
+
+	runes, err := utf8.ReadAll(utf8.NewReaderReader(repeater1))
+	assert.NoError(t, err, "reading runes")
+
+	expectedBytes, err := io.ReadAll(repeater2)
+	assert.NoError(t, err, "reading expected runes")
+
+	expected := []rune(string(expectedBytes))
+
+	assert.EqualInts(t, len(expected), len(runes), "length mismatch")
+
+	for j := 0; j < len(expected); j++ {
+		if expected[j] != runes[j] {
+			t.Errorf("rune %d are not equal: %c != %c",
+				j, expected[j], runes[j])
+		}
+	}
+}
